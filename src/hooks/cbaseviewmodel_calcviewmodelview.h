2b4d184fa1dc8e8b30ac2a9cc916d003e0858be0
uosq <lboxwallytf2@gmail.com>
2026-01-27 00:05:16 -0300

    Make CalcViewModelView use a way less horrible sig

--- src/hooks/cbaseviewmodel_calcviewmodelview.h
+++ src/hooks/cbaseviewmodel_calcviewmodelview.h
@@ -65,7 +65,7 @@ inline void HookedCalcViewModelView(void* thisptr, CBaseEntity* owner, const Vec
 
 inline void HookCalcViewModelView()
 {
-	originalCalcViewModelView = (CalcViewModelViewFn)(sigscan_module("client.so", "55 48 89 E5 41 57 41 56 41 55 49 89 F5 41 54 49 89 FC 53 48 83 EC 48 8B 41 08 48 8B 31 89 45 A8 89 45 B4 48 8B 02 48 89 75 A0 48 89 75 AC 48 89 45 B8 8B 42 08 8B 97 4C 0C"));
+	originalCalcViewModelView = (CalcViewModelViewFn)(sigscan_module("client.so", "55 48 89 E5 41 57 41 56 41 55 49 89 F5 41 54 49 89 FC 53 48 83 EC 48 8B 41 08"));
 	detour_init(&calcViewModel_ctx, (void*)originalCalcViewModelView, (void*)&HookedCalcViewModelView);
 	detour_enable(&calcViewModel_ctx);
 
